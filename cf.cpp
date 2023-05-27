e1ae64c2bc711814aba651fc6ae31806020f0171
afu114514 <134707870+afu114514@users.noreply.github.com>
2023-05-27 19:27:31 +0800

    Add files via upload

---
+++ cf.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long t,a,b;
+int main()
+{
+	cin>>t;
+	while(t--)
+	{
+		cin>>a>>b;
+		if(a%b==0)
+		{
+			cout<<2<<endl;
+			cout<<a-1<<" "<<1<<endl;
+		}
+		else 
+		{
+			cout<<1<<endl;
+			cout<<a<<endl;
+		}
+	}
+	return 0;
+}
