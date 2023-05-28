77b6f7f3d8eac3d91004e7cd5d4404e6dc22d710
afu114514 <134707870+afu114514@users.noreply.github.com>
2023-05-29 00:48:08 +0800

    Create 0006.A.cpp
    (brute, force ,geometry , 900)

---
+++ 0006.A.cpp
@@ -0,0 +1,16 @@
+
+  #include<bits/stdc++.h>
+using namespace std;
+int a[10];
+int main()
+{
+	for(int i=1;i<=4;i++)
+	{
+		cin>>a[i];
+	}
+	sort(a+1,a+5);
+	if(a[1]+a[2]>a[3]||a[2]+a[3]>a[4])printf("TRIANGLE\n");
+	else if(a[1]+a[2]==a[3]||a[2]+a[3]==a[4]) printf("SEGMENT\n");
+	else printf("IMPOSSIBLE\n");
+	return 0;
+}
