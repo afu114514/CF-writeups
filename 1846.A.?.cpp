9e0df1c41a52ee347b69c1832e3eb21ee2fa3c10
afu114514 <134707870+afu114514@users.noreply.github.com>
2023-07-10 21:09:55 +0800

    Create 1846.A.?.cpp
    (implementation , math)

---
+++ 1846.A.?.cpp
@@ -0,0 +1,22 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+long long t,n;
+int main()
+{
+	cin>>t;
+	while(t--)
+	{
+		long long a,b,ans=0;
+		cin>>n;
+		for(long long i=1;i<=n;i++)
+		{
+			cin>>a>>b;
+			if(a>b)
+			ans++;
+		}
+		cout<<ans<<endl;
+	}
+	return 0;
+}
+  
