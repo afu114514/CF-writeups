2513f8faa25a761d3ef37609719ebb146eef5aba
afu114514 <134707870+afu114514@users.noreply.github.com>
2023-07-18 20:36:18 +0800

    Create 158.A.cpp
    (*special problem , implementation ,800)

---
+++ 158.A.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,k,a[105],ans;
+
+int main()
+{
+//	freopen("data.in","r",stdin);
+	scanf("%d%d",&n,&k);
+	for(int i=1;i<=n;i++)
+	{
+		scanf("%d",&a[i]);
+		
+	}
+	for(int i=1;i<=k;i++)
+	{
+		if(a[i]>0)
+		ans++;
+		else
+		{
+			cout<<ans<<endl;
+			return 0;
+		}
+	}
+	for(int i=k+1;i<=n;i++)
+	{
+		if(a[i]==a[k])
+		{
+			ans++;
+		}
+		else
+		{
+			cout<<ans<<endl;
+			return 0;
+		}
+	}
+	cout<<ans<<endl;
+	return 0;
+
+}
