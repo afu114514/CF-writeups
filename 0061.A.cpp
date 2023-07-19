a6cde97357f57ea1f9f69fa8bcc165d68ac9aed0
afu114514 <134707870+afu114514@users.noreply.github.com>
2023-07-19 22:02:22 +0800

    Create 0061.A.cpp
    ( implementation ,800)

---
+++ 0061.A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+//	freopen("data.in","r",stdin);
+//	freopen("data.out","w",stdout);
+	string a,b,c;
+	cin>>a>>b;
+	for(int i=0;i<a.length();i++)
+	{
+		cout<<(a[i]!=b[i])?1:0;
+	
+	}
+	printf("\n");
+
+	return 0;
+}
