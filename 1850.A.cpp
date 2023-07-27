cea86cb1fd704d1afa84710b8f3eda26feb67f0c
afu114514 <134707870+afu114514@users.noreply.github.com>
2023-07-27 11:30:21 +0800

    Create 1850.A.cpp
    ( implementation ,sortings ,800)

---
+++ 1850.A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long n,a,b,c;
+int main()
+{
+	//freopen("data.in","r",stdin);
+	cin>>n;
+	while(n--)
+	{
+		cin>>a>>b>>c;
+		if(a+b>=10||a+c>=10||b+c>=10)
+		{
+			cout<<"YES"<<endl;
+		}
+		else
+		{
+			cout<<"NO"<<endl;
+		}
+	}
+	return 0;
+}
