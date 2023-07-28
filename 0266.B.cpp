0e3ecca336774917099f52dcb70e882f5be3e5b9
afu114514 <134707870+afu114514@users.noreply.github.com>
2023-07-28 12:52:39 +0800

    Create 0266.B.cpp
    (constructive algorithms ,graph ,matchings ,implementation ,shortest paths ,800)

---
+++ 0266.B.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+char a[105];
+int n,t;
+int main()
+{
+//	freopen("data.in","r",stdin);
+	cin>>n>>t;
+	for(int i=1;i<=n;i++)
+	{
+		cin>>a[i];
+	}
+	while(t--)
+	{
+		for(int i=1;i<=n;i++)
+		{
+			if(a[i]=='B'&&a[i+1]=='G')
+			{
+			swap(a[i],a[i+1]);
+			i++;
+		}
+		}
+	}
+	for(int i=1;i<=n;i++)
+	cout<<a[i];
+
+	return 0;
+
+}
